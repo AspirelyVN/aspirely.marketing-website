9c98160e122f30e982b36bd4426d1374fd8eddc7
Kim Nguyen <nghgkim.forwork@gmail.com>
2025-07-30 02:36:28 +0700

    Update middleware: Set cache-control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,23 @@
 import createMiddleware from 'next-intl/middleware';
-import { NextResponse } from 'next/server';
-import {routing} from './i18n/routing';
- 
-export default createMiddleware(routing);
+import { NextRequest } from 'next/server';
+import { routing } from './i18n/routing';
 
-export async function middleware() {
-  const res = NextResponse.next();
+const intlMiddleware = createMiddleware(routing);
+
+export function middleware(req: NextRequest) {
+  const res = intlMiddleware(req);
+
+  if (res instanceof Promise) {
+    return res.then((r) => {
+      r.headers.set('Cache-Control', 'public, max-age=3600, stale-while-revalidate=86400');
+      return r;
+    });
+  }
 
   res.headers.set('Cache-Control', 'public, max-age=3600, stale-while-revalidate=86400');
-
   return res;
 }
- 
+
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
 };
