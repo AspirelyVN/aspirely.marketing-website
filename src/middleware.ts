85264da2a1c28af7313faa7351e0d3487b857d0b
Kim Nguyen <nghgkim.forwork@gmail.com>
2025-07-30 02:28:57 +0700

    Update middleware: Set cache-control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 import {routing} from './i18n/routing';
  
 export default createMiddleware(routing);
 
-export async function middleware(req: NextRequest) {
+export async function middleware() {
   const res = NextResponse.next();
 
   res.headers.set('Cache-Control', 'public, max-age=3600, stale-while-revalidate=86400');
