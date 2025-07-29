f3333218e5ef20692daccf4eea9ff4c2f4b66dfc
Kim Nguyen <nghgkim.forwork@gmail.com>
2025-07-30 01:28:00 +0700

    Update homepage: Improve performance

--- src/components/TutorCard.tsx
+++ src/components/TutorCard.tsx
@@ -39,7 +39,7 @@ export default function TutorCard({
             src={image}
             alt={`Tutor ${id}`}
             fill
-            // priority
+            priority
             sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
             quality={80}
             className="object-cover"
